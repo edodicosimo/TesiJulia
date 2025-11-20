a65074511d3fa0f13657a59ee5eec64afe03c7a4
Edoardo Di Cosimo <162701242+edodicosimo@users.noreply.github.com>
2025-11-20 19:22:35 +0100

    remove boilerplate

--- src/ClusterSimulation.jl
+++ src/ClusterSimulation.jl
@@ -18,46 +18,9 @@ struct clCluster
     u :: Vector
 end
 
-
 struct clSample
     allclusters::Array{clCluster,1}
     N::Integer
-end
-
-
-# DATA SIMULLATION
-G = 50
-Ng = 1000
-N = G * Ng
-mu_g = randn(G)
-β = 2
-
-# s = sqrt.(abs.(mu_g.+10)) #questo 10 qua è v_0
-# a = mu_g ./ s
-# b = max.(s .^ 2 .- a.^2, 0)
-# Z1 = randn(G, Ng)
-# Z2 = randn(G, Ng)
-# Xig = reshape(s .* Z1, (N,1))
-# uig = reshape(a .* Z1 .+ b .* Z2,(N,1))
-
-# y_ig =  Xig .* β .+ uig
-
-# β_ols = (inv(Xig' * Xig) * (Xig' * y_ig))[1][1]
-
-# u_hat = y_ig .- β_ols .*Xig
-
-# Xg = reshape(Xig,Ng,G) #In ogni colonna c'è un cluster 
-# yg = reshape(y_ig, Ng, G)
-
-# β_g = mean(diag(inv(Xg' * Xg) * (Xg' * yg)))
-
-function sample()
-    df = DataFrame(
-        "Xig" => reshape(Xig,N,),
-        "y_ig" => reshape(y_ig,N,),
-        "uig" => reshape(uig,N,),
-        "u_hat" => reshape(u_hat,N,)
-        )
 end
 
 function sample(p::clPopulation, Ng::Int)
@@ -93,4 +56,4 @@ end
 
 s = clPopulation(50, randn(50), 2)
 
-typeof(sample(s,100)[1])
+sample(s,100)
