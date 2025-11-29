5284dad0bd4356e8af9f0336bc9c0110f2aa3fc2
Edoardo Di Cosimo <162701242+edodicosimo@users.noreply.github.com>
2025-11-29 16:55:53 +0100

    add test to check if expected score is 0 in he

--- test/test.jl
+++ test/test.jl
@@ -13,6 +13,13 @@ include("../src/ClusterSimulation.jl")
         hes = sample(hep,100)
         @test isa(hes,clSample)
     end
+
+    @testset "Expetcet score is 0" begin
+        X = getRegressorNoIntercept.(hes.allclusters)
+        U =  getU.(hes.allclusters)
+        iszero = .≈(mean.(broadcast((x,u) -> x .* u, X,U)),0;atol=0.2)
+    end
+
     @testset "Consistency Test" begin
         betahat = (bols.(sample(hep,1000).allclusters))
         consistentat01 = .≈(permutedims(stack(betahat))[:,2] - beta1 , 0; atol=0.1)
@@ -25,14 +32,15 @@ end
         beta2 = 2
 
         clp = clPopulation(50, randn(50), beta2)
-        @test isa(slp,clPopulation)
+        @test isa(clp,clPopulation)
 
-        cls = sample(s,1000)
+        cls = sample(clp,1000)
         @test isa(cls, clSample)
-    end
-    @testset "Consistency Test" begin
-        consistentat04 = .≈(permutedims(stack(bols.(cls.allclusters)))[:,2] .- beta2, 0 ; atol=0.4)
-        @test all(consistentat04)
+    
+        @testset "Consistency Test" begin
+            consistentat04 = .≈(permutedims(stack(bols.(cls.allclusters)))[:,2] .- beta2, 0 ; atol=0.4)
+            @test all(consistentat04)
+        end
     end
 
     @testset "Montecarlo" begin
@@ -76,3 +84,47 @@ plot(
     lw = 2,
     fillalpha = 0.3,
 )
+ ## Test that the score is 0 in every cluster
+
+x = ccc.X[:,2]
+u = ccc.u 
+
+print( ccc.df )
+
+mean(x)
+std(x)
+
+histogram(
+    x,
+    bins = 5000
+)
+
+histogram2d(x, u,
+    nbins=50,
+    xlabel="X",
+    ylabel="u",
+    title="2D Histogram")
+
+mean( x .* u )
+
+reduce(vcat, getRegressorNoIntercept.( hes.allclusters ) )
+
+function getRegressorNoIntercept(c::clCluster)
+    c.X[:,2]
+end
+
+function checkScore(s::clSample)
+    getRegressorNoIntercept.(s.allclusters) .* getU.(s.allclusters)
+end
+
+function getU(c::clCluster)
+    c.u
+end
+
+X = getRegressorNoIntercept.(hes.allclusters)
+U =  getU.(hes.allclusters)
+
+histogram(
+    mean.(broadcast((x,u) -> x .* u, X,U)),
+    bins=50
+    )
